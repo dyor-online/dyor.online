3d88ecf428a5e91227d57982354b16c5aeeea1b9
jgdesert <jg.desert@dfree.fr>
2020-08-01 18:51:08 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,96 @@
+var camera, scene, renderer,
+    geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+    clock = new THREE.Clock();
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+    scene.add( camera );
+
+    geometry = new THREE.CubeGeometry( 200, 200, 200 );
+    material = new THREE.MeshLambertMaterial( { color: 0xfce703, wireframe: false } );
+    mesh = new THREE.Mesh( geometry, material );
+    //scene.add( mesh );
+    cubeSineDriver = 0;
+
+    textGeo = new THREE.PlaneGeometry(300,300);
+    THREE.ImageUtils.crossOrigin = ''; //Need this to pull in crossdomain images from AWS
+    textTexture = THREE.ImageUtils.loadTexture('https://skydrain.net/JADSq3esKAEOGrmzteDwDmIEDcGPzQPDGqqY_pDobDlD8A');
+    textMaterial = new THREE.MeshLambertMaterial({color: 0x0f56ba, opacity: 1, map: textTexture, transparent: true, blending: THREE.AdditiveBlending})
+    text = new THREE.Mesh(textGeo,textMaterial);
+    text.position.z = 800;
+    scene.add(text);
+
+    light = new THREE.DirectionalLight(0xffffff,0.5);
+    light.position.set(-1,0,1);
+    scene.add(light);
+
+    smokeTexture = THREE.ImageUtils.loadTexture('https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png');
+    smokeMaterial = new THREE.MeshLambertMaterial({color: 0x0f56ba, map: smokeTexture, transparent: true});
+    smokeGeo = new THREE.PlaneGeometry(300,300);
+    smokeParticles = [];
+
+
+    for (p = 0; p < 150; p++) {
+        var particle = new THREE.Mesh(smokeGeo,smokeMaterial);
+        particle.position.set(Math.random()*500-250,Math.random()*500-250,Math.random()*1000-100);
+        particle.rotation.z = Math.random() * 360;
+        scene.add(particle);
+        smokeParticles.push(particle);
+    }
+
+    document.body.appendChild( renderer.domElement );
+    /**/
+    heightOutput = document.querySelector('#height');
+    widthOutput = document.querySelector('#width');
+    document.body.addEventListener("click", function (event) {
+      if (event.target.classList.contains("delete")) {
+        scene.remove(text);
+      }
+      if (event.target.classList.contains("addLogo")) {
+        scene.add(text);
+      }
+    })
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+function animate() {
+
+    // note: three.js includes requestAnimationFrame shim
+    window.addEventListener( 'resize', onWindowResize, false );
+    delta = clock.getDelta();
+    requestAnimationFrame( animate );
+    evolveSmoke();
+    render();
+}
+
+function evolveSmoke() {
+    var sp = smokeParticles.length;
+    while(sp--) {
+        smokeParticles[sp].rotation.z += (delta * 0.2);
+    }
+}
+
+function render() {
+
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.01;
+    cubeSineDriver += .01;
+    mesh.position.z = 100 + (Math.sin(cubeSineDriver) * 500);
+    renderer.render( scene, camera );
+
+}
